7c05d3b6eb0d222c90ef0886393c40ff30522215
KevinStirling <youngks93@gmail.com>
2022-01-30 15:51:43 -0500

    user and home post feeds

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,54 @@
-export default function UserPage({}) {
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
+
+import {
+  collection,
+  getDocs,
+  orderBy,
+  query,
+  where,
+  limit,
+  getFirestore,
+} from "firebase/firestore";
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+
+export async function getServerSideProps({ query: urlQuery }) {
+  const { username } = urlQuery;
+
+  const userDoc = await getUserWithUsername(username);
+
+  if (!userDoc) {
+    return {
+      notFound: true,
+    };
+  }
+
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+
+    const postsQuery = query(
+      collection(getFirestore(), userDoc.ref.path, "posts"),
+      where("published", "==", true),
+      orderBy("createdAt", "desc"),
+      limit(5)
+    );
+
+    posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts },
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
   return (
     <main>
-      <h1>Hello </h1>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </main>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,85 @@ import styles from "../styles/Home.module.css";
 
 import Loader from "../components/Loader";
 import toast from "react-hot-toast";
+import {
+  collectionGroup,
+  getDocs,
+  getFirestore,
+  orderBy,
+  limit,
+  startAfter,
+  query,
+  where,
+  Timestamp,
+} from "firebase/firestore";
+import { postToJSON } from "../lib/firebase";
+import { useState } from "react";
+import PostFeed from "../components/PostFeed";
 
-export default function Home() {
+// max post to query per page
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const ref = collectionGroup(getFirestore(), "posts");
+  const postsQuery = query(
+    ref,
+    where("published", "==", true),
+    orderBy("createdAt", "desc"),
+    limit(LIMIT)
+  );
+
+  const posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+
+  return {
+    props: { posts },
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+    const ref = collectionGroup(getFirestore(), "posts");
+
+    const cursor =
+      typeof last.createdAt === "number"
+        ? Timestamp.fromMillis(last.createdAt)
+        : last.createdAt;
+
+    const postsQuery = query(
+      ref,
+      where("published", "==", true),
+      orderBy("createdAt", "desc"),
+      startAfter(cursor),
+      limit(LIMIT)
+    );
+
+    const newPosts = (await getDocs(postsQuery)).docs.map(postToJSON);
+
+    setPosts(posts.concat(newPosts));
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+
+    setLoading(false);
+  };
+
   return (
-    <div>
-      <button onClick={() => toast.success("hello toast")}>Toast Me</button>
-    </div>
+    <main>
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load More</button>
+      )}
+      <Loader show={loading} />
+
+      {postsEnd && "You have reached the end!"}
+    </main>
   );
 }
